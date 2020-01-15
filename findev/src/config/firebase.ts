94507a57d9a6c4c88c2e8884d7360558d4ac32db
rodrigo <rodrigo.arantes@totvs.com.br>
2020-01-15 16:19:10 -0300

    #findev heroku

--- findev/src/config/firebase.ts
+++ findev/src/config/firebase.ts
@@ -1,7 +1,7 @@
 import { ServiceAccount } from 'firebase-admin';
 
 const firebase = () => {
-  const firebaseConfig: string = process.env.FIREBASE || '';
+  const firebaseConfig: string = process.env.FIREBASE || '{}';
   const serviceAccount: ServiceAccount = JSON.parse(firebaseConfig);
   return serviceAccount;
 };
