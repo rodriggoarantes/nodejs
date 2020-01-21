e14a607947f48d9e2bab7505b18f8af76f496dc3
rodrigo <rodrigo.arantes@totvs.com.br>
2020-01-21 14:43:31 -0300

    ajustes de rotas

--- devradar/src/app/controllers/UserController.ts
+++ devradar/src/app/controllers/UserController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 
 class UserController {
-  async hello(req: Request, res: Response) {
+  async index(req: Request, res: Response) {
     return res.json({ id: 1, name: 'rodrigo', email: 'hello@hello.com' });
   }
 }
--- devradar/src/routes.ts
+++ devradar/src/routes.ts
@@ -1,20 +1,25 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
 import authMiddleware from '@app/middleware/auth';
 
 import UserController from '@app/controllers/UserController';
 import DevController from '@app/controllers/DevController';
 import SearchController from '@app/controllers/SearchController';
+import StatusController from '@app/controllers/StatusController';
 
 const routes = Router();
 
-routes.get('/hello', UserController.hello);
+routes.get('', (_: Request, res: Response) => {
+  res.redirect('/status');
+});
+routes.get('/status', StatusController.status);
 
 routes.get('/devs', DevController.index);
 routes.post('/devs', DevController.store);
+
 routes.post('/search', SearchController.index);
 
 routes.use(authMiddleware);
 
-routes.get('/loggedin', UserController.hello);
+routes.get('/user', UserController.index);
 
 export default routes;
